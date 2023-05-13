d6391189e97367a74c5afc3f197735627b849b02
ferreiraweb <ajferreira.br@hotmail.com>
2023-05-12 22:00:08 -0300

    first commit - app-call

---
+++ src/styles/global.ts
@@ -0,0 +1,18 @@
+import { globalCss } from ".";
+
+
+export const globalStyles = globalCss({
+
+    '*': {
+        padding: 0,
+        margin: 0,
+        boxSizing: 'border-box'
+    },
+
+    body: {
+        backgroundColor: '$gray-900',
+        color: "$gray-100"
+    },
+    
+    
+})
