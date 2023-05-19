c0e061b0ebe41b3b26a4e0a818e3f8362091728c
ferreiraweb <ajferreira.br@hotmail.com>
2023-05-18 21:57:38 -0300

    react-hook-form (1)

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,12 +1,35 @@
 import { ArrowArcRight, ArrowLineRight } from "phosphor-react";
 import { Button, Form, TextInput } from "./styles";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+
+const claimUsernameFormSchema = z.object({
+    username: z.string()
+})
+
+type claimUserNameFormType = z.infer<typeof claimUsernameFormSchema>
+
+
 
 
 export function ClaimUsernamForm() {
+
+
+    const {register, handleSubmit} = useForm<claimUserNameFormType>();
+
+    async function handleSubmitForm(data: claimUserNameFormType) {
+
+        console.log(data)
+
+    }
+
+
+
     return (
-        <Form>
-            <TextInput />
-            <Button>
+        <Form onSubmit={handleSubmit(handleSubmitForm)}>
+            <TextInput  {...register("username")} />
+            <Button type="submit">
                 <span>Reservar</span>   
                 <ArrowLineRight />
             </Button>
